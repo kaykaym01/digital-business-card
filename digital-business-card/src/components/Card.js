15cbb7c3e83b43f76604988f790c410a9b1dc8ab
kaykaym01 <kaymoore01@gmail.com>
2022-08-31 14:26:58 -0400

    Fix broken link to components

--- digital-business-card/src/components/Card.js
+++ digital-business-card/src/components/Card.js
@@ -1,10 +1,11 @@
-import Header from "./components/Header";
-import Main from "./components/Main";
-import Footer from "./components/Footer";
+import Header from "./Header";
+import Main from "./Main";
+import Footer from "./Footer";
 
 function Card() {
   return (
     <div className="card">
+        <h1>THis is a card</h1>
       <Header />
       <Main />
       <Footer />
