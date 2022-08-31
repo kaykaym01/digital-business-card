1c7160dd9bb1467a8604154c364afdbff8b40414
kaykaym01 <kaymoore01@gmail.com>
2022-08-31 14:25:12 -0400

    Add card component

--- digital-business-card/src/App.js
+++ digital-business-card/src/App.js
@@ -1,14 +1,10 @@
 import './App.css';
-import Header from "./components/Header";
-import Main from "./components/Main";
-import Footer from "./components/Footer";
+import Card from "./components/Card";
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Main />
-      <Footer />
+     <Card />
     </div>
   );
 }
---
+++ digital-business-card/src/components/Card.js
@@ -0,0 +1,15 @@
+import Header from "./components/Header";
+import Main from "./components/Main";
+import Footer from "./components/Footer";
+
+function Card() {
+  return (
+    <div className="card">
+      <Header />
+      <Main />
+      <Footer />
+    </div>
+  );
+}
+
+export default Card;
