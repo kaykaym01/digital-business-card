925fc650fdf5aa3510b3a0e7f7c6dcf8ee61e170
kaykaym01 <kaymoore01@gmail.com>
2022-09-13 01:27:34 -0400

    Set up header with name, role, and contact links

---
+++ digital-business-card/src/components/Header.js
@@ -0,0 +1,25 @@
+import profileImg from "../img/profile-image.jpg";
+
+function Header() {
+  return (
+    <header className="header">
+      <div className="header-img">
+        <img src={profileImg} alt="Kayla Moore" className="profile-img"></img>
+      </div>
+      <div className="header-text">
+        <h1 className="name">Kayla Moore</h1>
+        <p className="role">Frontend Developer</p>
+      </div>
+      <div className="header-links">
+        <a href="mailto:kaymoore01@gmail.com" className="contact-link" id="email-link">
+          Email
+        </a>
+        <a href="https://www.linkedin.com/in/kayla-moore/" className="contact-link" id="linkedin-link" target="_blank" rel="noopener noreferrer">
+          LinkedIn
+        </a>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
