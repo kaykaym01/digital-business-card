be839e63247a7581bd6b1f90ca1f68a33d405170
kaykaym01 <kaymoore01@gmail.com>
2022-09-13 01:33:41 -0400

    set up main component with about and interests section

---
+++ digital-business-card/src/components/Main.js
@@ -0,0 +1,24 @@
+function Main() {
+  return (
+    <main className="main">
+      <div className="about">
+        <h2>About</h2>
+        <p>
+          I am a frontend developer with a particular interest in making things
+          simple and automating daily tasks. I try to keep up with security and
+          best practices, and am always looking for new things to learn.
+        </p>
+      </div>
+
+      <div className="interests">
+        <h2>Interests</h2>
+        <p>
+          Traveler. Reader. Writer. History lover. Podcast listener.
+          Rollerskater. Tiny desk concert enthusiast.
+        </p>
+      </div>
+    </main>
+  );
+}
+
+export default Main;
