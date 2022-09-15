8fbd593f0298f552a07b30ea3bad3dc08fc54090
kaykaym01 <kaymoore01@gmail.com>
2022-09-14 22:39:48 -0400

    Align Content component within Card component

--- digital-business-card/src/components/Card.js
+++ digital-business-card/src/components/Card.js
@@ -1,11 +1,13 @@
 import ProfileImage from "./ProfileImage";
 import Content from "./Content";
+import Footer from "./Footer";
 
 function Card() {
   return (
     <div className="card">
       <ProfileImage />
       <Content />
+      <Footer />
     </div>
   );
 }
--- digital-business-card/src/components/Content.js
+++ digital-business-card/src/components/Content.js
@@ -1,13 +1,11 @@
 import Header from "./Header";
 import Main from "./Main";
-import Footer from "./Footer";
 
 function Content() {
   return (
     <div className="content">
       <Header />
       <Main />
-      <Footer />
     </div>
   );
 }
