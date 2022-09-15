3eec01e5daeb97a318257405fed55ee1336cca23
kaykaym01 <kaymoore01@gmail.com>
2022-09-14 20:14:36 -0400

    Align and add formatting to main sections

--- digital-business-card/src/components/Main.js
+++ digital-business-card/src/components/Main.js
@@ -1,18 +1,18 @@
 function Main() {
   return (
     <main className="main">
-      <div className="about">
-        <h2>About</h2>
-        <p>
+      <div className="main-section">
+        <h2 className = "main-sub-header">About</h2>
+        <p className="main-text">
           I am a frontend developer with a particular interest in making things
           simple and automating daily tasks. I try to keep up with security and
           best practices, and am always looking for new things to learn.
         </p>
       </div>
 
-      <div className="interests">
-        <h2>Interests</h2>
-        <p>
+      <div className="main-section">
+        <h2 className = "main-sub-header">Interests</h2>
+        <p className="main-text">
           Traveler. Reader. Writer. History lover. Podcast listener.
           Rollerskater. Tiny desk concert enthusiast.
         </p>
