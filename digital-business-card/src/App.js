c052e9711b705c52a510b9e390f81856fdba691a
kaykaym01 <kaymoore01@gmail.com>
2022-09-15 17:50:47 -0400

    Add CardWrapper component with padding around Card component

--- digital-business-card/src/App.js
+++ digital-business-card/src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
-import Card from "./components/Card";
+import CardWrapper from "./components/CardWrapper";
 
 function App() {
   return (
     <div className="App">
-     <Card />
+     <CardWrapper />
     </div>
   );
 }
---
+++ digital-business-card/src/components/CardWrapper.js
@@ -0,0 +1,11 @@
+import Card from "./Card";
+
+function CardWrapper() {
+  return (
+    <div className="card-wrapper">
+     <Card />
+    </div>
+  );
+}
+
+export default CardWrapper;
